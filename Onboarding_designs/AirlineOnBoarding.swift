be1622380d15b0ba268683f29752f564ee52ccd8
Dev Patel <dpatel222430@gmail.com>
2024-06-07 01:26:57 +0530

    Started

---
+++ Onboarding_designs/AirlineOnBoarding.swift
@@ -0,0 +1,18 @@
+//
+//  AirlineOnBoarding.swift
+//  Onboarding_designs
+//
+//  Created by Dev Patel on 07/06/24.
+//
+
+import SwiftUI
+
+struct AirlineOnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AirlineOnBoarding()
+}
